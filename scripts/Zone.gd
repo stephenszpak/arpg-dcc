ca9c3156ee09e3d1194c64e543a581fe8606daab
Stephen Szpak <srszpak@gmail.com>
2025-07-15 14:50:05 -0500

    Replace deprecated OS.get_ticks_msec with Time

--- scripts/Zone.gd
+++ scripts/Zone.gd
@@ -6,7 +6,7 @@ var idle_timeout := 2.0
 func spawn_entity(id: int):
     var npc := CharacterBody3D.new()
     add_child(npc)
-    entities[id] = {"node": npc, "last_move": OS.get_ticks_msec()/1000.0}
+    entities[id] = {"node": npc, "last_move": Time.get_ticks_msec()/1000.0}
 
 func remove_entity(id:int):
     if entities.has(id):
@@ -17,10 +17,10 @@ func move_entity(id:int, pos:Vector3):
     if entities.has(id):
         var info = entities[id]
         info["node"].global_transform.origin = info["node"].global_transform.origin.lerp(pos, 0.25)
-        info["last_move"] = OS.get_ticks_msec()/1000.0
+        info["last_move"] = Time.get_ticks_msec()/1000.0
 
 func _process(delta):
-    var t = OS.get_ticks_msec()/1000.0
+    var t = Time.get_ticks_msec()/1000.0
     for id in entities.keys():
         var info = entities[id]
         if t - info["last_move"] > idle_timeout:
