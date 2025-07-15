e38525bf69c6e0c360bbe5e37d253fa6b858fd13
Stephen Szpak <stephenszpak@Stephens-MacBook-Air.local>
2025-07-15 15:46:49 -0500

    more stUFF

--- scripts/Zone.gd
+++ scripts/Zone.gd
@@ -4,25 +4,25 @@ var entities := {}
 var idle_timeout := 2.0
 
 func spawn_entity(id: int):
-    var npc := CharacterBody3D.new()
-    add_child(npc)
-    entities[id] = {"node": npc, "last_move": Time.get_ticks_msec()/1000.0}
+	var npc := CharacterBody3D.new()
+	add_child(npc)
+	entities[id] = {"node": npc, "last_move": Time.get_ticks_msec()/1000.0}
 
 func remove_entity(id:int):
-    if entities.has(id):
-        entities[id]["node"].queue_free()
-        entities.erase(id)
+	if entities.has(id):
+		entities[id]["node"].queue_free()
+		entities.erase(id)
 
 func move_entity(id:int, pos:Vector3):
-    if entities.has(id):
-        var info = entities[id]
-        info["node"].global_transform.origin = info["node"].global_transform.origin.lerp(pos, 0.25)
-        info["last_move"] = Time.get_ticks_msec()/1000.0
+	if entities.has(id):
+		var info = entities[id]
+		info["node"].global_transform.origin = info["node"].global_transform.origin.lerp(pos, 0.25)
+		info["last_move"] = Time.get_ticks_msec()/1000.0
 
 func _process(delta):
-    var t = Time.get_ticks_msec()/1000.0
-    for id in entities.keys():
-        var info = entities[id]
-        if t - info["last_move"] > idle_timeout:
-            # TODO: play idle animation
-            pass
+	var t = Time.get_ticks_msec()/1000.0
+	for id in entities.keys():
+		var info = entities[id]
+		if t - info["last_move"] > idle_timeout:
+			# TODO: play idle animation
+			pass
